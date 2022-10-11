a236ab517126b03d2220b1e69775f93ebdf7978e
ALINE <hoshinoaline@yahoo.com.br>
2022-10-10 22:33:37 -0300

    rota get conectada

--- front-end/src/components/Lista/Lista.tsx
+++ front-end/src/components/Lista/Lista.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-key */
 import { Button } from '@mui/material';
 import { Professor } from '../../@types/professor';
+import { FormatadorService } from '../../services/FormatadorService';
 import { Foto, ItemLista, ListaStyled, Informacoes, Nome, Valor, Descricao, ListaVazia } from "./Lista.style";
 
 interface ListaProps {
@@ -17,8 +18,8 @@ const Lista = (props: ListaProps) => {
                             <Foto src={professor.foto}></Foto>
                             <Informacoes>
                                 <Nome>{professor.nome}</Nome>
-                                <Valor>{professor.valor_hora.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL'})} por hora</Valor>
-                                <Descricao>{professor.descricao}</Descricao>
+                                <Valor>{FormatadorService.valorMonetario(professor.valor_hora)} por hora</Valor>
+                                <Descricao>{FormatadorService.limitarTexto(professor.descricao, 200)}</Descricao>
                                 <Button sx={{ width: '70%'}}>Marcar Aula com Aline</Button>
                             </Informacoes>
                         </ItemLista>
